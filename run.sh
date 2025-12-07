ce4e4751ed42ae67fd28edbba1db4af99e80b660
Andrea Alfonsi <andrea.alfonsi@protonmail.com>
2025-12-07 09:36:45 +0000

    Added code genration for python and organized typescript

--- run.sh
+++ run.sh
@@ -1,12 +1,19 @@
-docker build -t avro-build .
-docker run --rm \
-  -v "$PWD/src:/work/avdl" \
-  -v "$PWD/out:/work/out" \
-  avro-build
+# docker build -t avro-build .
+# docker run --rm \
+#   -v "$PWD/src:/work/avdl" \
+#   -v "$PWD/out:/work/out" \
+#   avro-build
 
 
-docker build -t avro-typescript  -f Dockerfile.typescript .
+docker build -t avro-codegen-typescript  -f typescript/Dockerfile .
 docker run --rm \
   -v "$PWD/out/avsc:/usr/src/app/schemas" \
   -v "$PWD/out/typescript:/usr/src/app/types" \
-  avro-typescript
+  avro-codegen-typescript
+
+
+docker build -t avro-codegen-python  -f python/Dockerfile .
+docker run --rm \
+  -v "$PWD/out/avsc:/app/schemas" \
+  -v "$PWD/out/python:/app/types" \
+  avro-codegen-python
